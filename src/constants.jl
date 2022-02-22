43df03fea2724eee250c45e0f01735bba275d8b8
Neel Smith <dnsmith.neel@gmail.com>
2022-02-22 14:40:30 -0500

    Assemble library header from template in `hmt-archive`

--- src/constants.jl
+++ src/constants.jl
@@ -1,25 +1,5 @@
+#
 # Some constant values:
-
+#
 """URN for a `DSECollection` with all HMT project DSE records."""
 DSE_URN = Cite2Urn("urn:cite2:hmt:hmtdse.v1:")
-
-
-"""Template string for CEX library header"""
-CEX_HEADER_SOURCE = """
-// Metadata for the current release
-
-#!cexversion
-3.0.2
-
-#!citelibrary
-// These values are inserted programmtacally when
-// the CITE library is built:
-name|Homer Multitext project, release RELEASE_ID
-urn|urn:cite2:hmt:publications.cex.RELEASE_ID:all
-license|Creative Commons Attribution, Non-Commercial 4.0 License <https://creat\
-ivecommons.org/licenses/by-nc/4.0/>.
-
-// CITE namespace definitions
-namespace|hmt|http://www.homermultitext.org/citens/hmt
-namespace|greekLit|http://chs.harvard.edu/ctsns/greekLit
-"""
