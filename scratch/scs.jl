01696443c0060cc5e966f0f5ac4de9c2e7ca7f6a
Neel Smith <dnsmith.neel@gmail.com>
2022-03-01 06:09:24 -0500

    save a dummy

--- scratch/scs.jl
+++ scratch/scs.jl
@@ -4,15 +4,16 @@ using CitableBase
 using CitableText
 using CitableCorpus
 c = fromcex(f, CitableTextCorpus, FileReader)
-i9 = CtsUrn("urn:cts:greekLit:tlg0012.tlg001:9")
-all9 = filter(p -> urncontains(i9, urn(p)), c.passages)
-norm9 = filter(p -> exemplarid(urn(p)) == "normalized", all9)
-urns9 = map(p -> urn(p), norm9)
+i10 = CtsUrn("urn:cts:greekLit:tlg0012.tlg001:10")
+
+all10 = filter(p -> urncontains(i10, urn(p)), c.passages)
+norm10 = filter(p -> exemplarid(urn(p)) == "normalized", all10)
+urns10 = map(p -> urn(p), norm10)
 
 
-aurns = filter(u -> versionid(u) == "msA", urns9)
-burns = filter(u -> versionid(u) == "msB", urns9)
-e3urns = filter(u -> versionid(u) == "e3", urns9)
+aurns = filter(u -> versionid(u) == "msA", urns10)
+burns = filter(u -> versionid(u) == "msB", urns10)
+e3urns = filter(u -> versionid(u) == "e3", ururns10ns9)
 
 astr = join(aurns)
 bstr = join(burns)
