96b2864f7b1ed3a3348f48464adbdbaaa525402f
Neel Smith <dnsmith.neel@gmail.com>
2023-06-06 04:01:02 -0400

    Add method for function hmt_commentary

--- src/cex/commentary.jl
+++ src/cex/commentary.jl
@@ -6,4 +6,9 @@ function hmt_commentary(src::AbstractString; delimiter = "|")
     fromcex(src, CitableCommentary, delimiter = delimiter)
 end
 
+function hmt_commentary()
+    hmt_cex() |> hmt_commentary
+end
+
+
 
