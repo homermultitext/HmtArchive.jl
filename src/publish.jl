78a2fe434fd42d6ba74a7a6fa91aff6304f6d34d
Neel Smith <dnsmith.neel@gmail.com>
2023-07-06 02:00:50 -0400

    Publish index of image regions to VA pages

--- src/publish.jl
+++ src/publish.jl
@@ -50,7 +50,8 @@ function librarycex(hmt::Archive, releaseid::AbstractString)
         persnameindexcex(archivaltexts),
         placenameindexcex(archivaltexts),
         ethnicgroupindexcex(archivaltexts),
-        relationsetscex(hmt)
+        relationsetscex(hmt),
+        pageroiscex(hmt)
         ], "\n\n")
 end
 
@@ -173,6 +174,21 @@ function remoteauthlists()
         push!(datacex, s)
     end
     catalogcex * "\n\n" * join(datacex, "\n\n")
+end
+
+
+"""Read index of image regions for pages of Venetus A manuscript (CEX format).
+$(SIGNATURES)
+"""
+function pageroiscex(hmt::Archive)
+    read(pageroisfile(hmt), String)
+end
+
+"""Find path to CEX file with index of image regions to pages of the Venetus A.
+$(SIGNATURES)
+"""
+function pageroisfile(hmt::Archive)
+    joinpath(hmt.root, "relations", "va-page-imagerois.cex")
 end
 
 """Read text catalog CEX.
