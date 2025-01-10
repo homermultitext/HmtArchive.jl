2652a56866e72e121c4a8d212028d941a1d7f54c
Documenter.jl <documenter@juliadocs.github.io>
2025-01-10 11:56:31 +0000

    build based on 66dd4f2

---
+++ v0.16.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HMTArchive.jl","page":"Home","title":"HMTArchive.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"HmtArchive is a Julia package for working with the citable content freely available from the Homer Multitext project's github repository for archival data at https://github.com/homermultitext/hmt-archive. ","category":"page"},{"location":"","page":"Home","title":"Home","text":"Documentation for the package is migrating to quarto.pub.","category":"page"}]
+}
